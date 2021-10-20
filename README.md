391e38b18d0fa345332eb594458a2b4540a2aa7c
Alessandro Peretti <alessandro.peretti@spindox.it>
2021-10-20 22:50:00 +0200

    :tada: first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### INSTALL REQUIREMENTS
+
+`pip install -r requirements.txt`
+
+### HOW TO USE IT
+
+`python3 coregistration.py`
+
+In `registerd.jpg` you will have the image obtained after the co-registration
---
+++ coregistration.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+
+
+
+master  = cv2.imread("./images/master.jpg") #Master
+slave = cv2.imread("./images/slave.jpg") #Slave
+
+# Convert to grayscale.
+img1 = cv2.cvtColor(master, cv2.COLOR_BGR2GRAY)
+img2 = cv2.cvtColor(slave, cv2.COLOR_BGR2GRAY)
+
+# get h and w of image
+height, width = img2.shape
+
+# Create ORB detector. FAST and BRIEF descriptor algorithm
+orb_detector = cv2.ORB_create(6000)
+ 
+# Find keypoints and descriptors.
+kp1, d1 = orb_detector.detectAndCompute(img1, None)
+kp2, d2 = orb_detector.detectAndCompute(img2, None)
+ 
+# Hamming distance.
+matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
+ 
+# two sets of descriptors.
+matches = matcher.match(d1, d2)
+ 
+# Sort matches on the basis of their Hamming distance.
+matches.sort(key = lambda x: x.distance)
+ 
+# Take the top 90 % matches forward.
+matches = matches[:int(len(matches)*0.9)]
+no_of_matches = len(matches)
+ 
+# Define empty matrices of shape no_of_matches * 2.
+p1 = np.zeros((no_of_matches, 2))
+p2 = np.zeros((no_of_matches, 2))
+ 
+for i in range(len(matches)):
+  p1[i, :] = kp1[matches[i].queryIdx].pt
+  p2[i, :] = kp2[matches[i].trainIdx].pt
+ 
+# homography matrix.
+homography, mask = cv2.findHomography(p1, p2, cv2.RANSAC)
+ 
+# warp with homography
+transformed_img = cv2.warpPerspective(master,
+                    homography, (width, height))
+ 
+# Save the image obtained.
+cv2.imwrite('registered.jpg', transformed_img)
